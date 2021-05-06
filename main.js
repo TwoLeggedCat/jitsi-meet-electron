a2c04c8229c02d301080d9bca1daa261a8ee2912
TwoLeggedCat <80929284+TwoLeggedCat@users.noreply.github.com>
2021-05-06 15:35:28 -0500

    [build] I hate you; leave my `if (false)` alone
    My sincere apologies to GitHub for wasting your time with that last build

--- main.js
+++ main.js
@@ -165,8 +165,8 @@ function createJitsiMeetWindow() {
     setApplicationMenu();
 
     // Check for Updates.
-    if (false && !process.mas) {
-        autoUpdater.checkForUpdatesAndNotify();
+    if (!process.mas) {
+        //autoUpdater.checkForUpdatesAndNotify();
     }
 
     // Load the previous window state with fallback to defaults.
